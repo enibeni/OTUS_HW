26e4f581642caf8161bf34193e0dfde62ca11a76
Sergey Morozov <se.mrzv@gmail.com>
2019-10-20 21:38:21 +0300

    add logger db handler

--- 05_selenium/log_helper.py
+++ 05_selenium/log_helper.py
@@ -1,8 +1,63 @@
 import os
 import time
 import logging
+import sqlite3
 from datetime import date
 from selenium.webdriver.support.event_firing_webdriver import AbstractEventListener
+
+
+initial_sql = """CREATE TABLE IF NOT EXISTS log(
+                    TimeStamp TEXT,
+                    FuncName TEXT,
+                    LogLevelName TEXT,
+                    Message TEXT,
+                    Exception TEXT
+               )"""
+
+insertion_sql = """INSERT INTO log(
+                    TimeStamp,
+                    FuncName,
+                    LogLevelName,
+                    Message,
+                    Exception
+               )
+               VALUES (
+                    '%(dbtime)s',
+                    '%(funcName)s',
+                    '%(levelname)s',
+                    '%(msg)s',
+                    '%(exc_text)s'
+               );
+               """
+
+
+class SQLiteLoggerHandler(logging.Handler):
+
+    def __init__(self, db='log.db'):
+        logging.Handler.__init__(self)
+        self.db = db
+        conn = sqlite3.connect(self.db)
+        conn.execute(initial_sql)
+        conn.commit()
+
+    def format_time(self, record):
+        """ Create a time stamp
+        """
+        record.dbtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created))
+
+    def emit(self, record):
+        self.format(record)
+        self.format_time(record)
+        if record.exc_info:  # for exceptions
+            record.exc_text = logging._defaultFormatter.formatException(record.exc_info)
+        else:
+            record.exc_text = ""
+
+        # Insert the log record
+        sql = insertion_sql % record.__dict__
+        conn = sqlite3.connect(self.db)
+        conn.execute(sql)
+        conn.commit()
 
 
 class SessionLogger:
@@ -27,6 +82,9 @@ class SessionLogger:
         console_format = logging.Formatter('%(levelname)-5s [%(asctime)s] [%(name)s]: %(message)s')
         console_log.setFormatter(console_format)
         logger.addHandler(console_log)
+
+        db_log = SQLiteLoggerHandler()
+        logger.addHandler(db_log)
         return logger
 
 
